6478a6cb56d00158ced74bca1b1520663313188f
Tan Ea Son <taneason0912@gmail.com>
2026-09-06 23:14:52 +0800

    fix: promote car to non-nullable local variable in builder closure

--- lib/services/snap_identify_helper.dart
+++ lib/services/snap_identify_helper.dart
@@ -135,12 +135,13 @@ class SnapIdentifyHelper {
         ),
       );
     } else if (car != null) {
+      final identifiedCar = car;
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Identified as ${car.fullName}!'), backgroundColor: AppColors.accentGreen),
+        SnackBar(content: Text('Identified as ${identifiedCar.fullName}!'), backgroundColor: AppColors.accentGreen),
       );
       Navigator.push(
         context,
-        MaterialPageRoute(builder: (_) => CarDetailScreen(car: car.toJson())),
+        MaterialPageRoute(builder: (_) => CarDetailScreen(car: identifiedCar.toJson())),
       );
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
